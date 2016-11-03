e71d9e2ab226dec86a8ac067881b781446d1fbc7
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2016-11-02 21:39:21 -0400

    Update indexes-to-element.js

--- indexes-to-element.js
+++ indexes-to-element.js
@@ -2,9 +2,11 @@ function previousSiblingCount(element) {
   if (!element) {
     return null;
   }
+  
   if (!element.previousSibling) {
     return 0;
   }
+  
   return previousSiblingCount(element.previousSibling) + 1;
 }
 
@@ -12,10 +14,12 @@ function indexesToElement(decendantElement, ancestorElement, indexes) {
   if (!decendantElement) {
     return [];
   }
+  
   ancestorElement = ancestorElement || document;
   if (decendantElement == ancestorElement) {
     return [];
   }
+  
   indexes = indexesToElement(decendantElement.parentNode, ancestorElement), indexes || []);
   indexes.push(previousSiblingCount(decendantElement));
   return indexes;
