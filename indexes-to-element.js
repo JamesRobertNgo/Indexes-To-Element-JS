644fe20b35a4af41acfd72896f21634f28a8e05a
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2016-11-02 21:36:41 -0400

    Create indexes-to-element.js

---
+++ indexes-to-element.js
@@ -0,0 +1,22 @@
+function previousSiblingCount(element) {
+  if (!element) {
+    return null;
+  }
+  if (!element.previousSibling) {
+    return 0;
+  }
+  return previousSiblingCount(element.previousSibling) + 1;
+}
+
+function indexesToElement(decendantElement, ancestorElement, indexes) {
+  if (!decendantElement) {
+    return [];
+  }
+  ancestorElement = ancestorElement || document;
+  if (decendantElement == ancestorElement) {
+    return [];
+  }
+  indexes = indexesToElement(decendantElement.parentNode, ancestorElement), indexes || []);
+  indexes.push(previousSiblingCount(decendantElement));
+  return indexes;
+}
